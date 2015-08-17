485e829cf03cdd1a90267816ed64c223d1d84912
Dan Russell <danr94@gmail.com>
2015-08-17 15:57:02 -0700

    Upon selecting a structure in the world view, a selection box is placed around the structure to help show the selection.

--- src/com/projects/view/WorldViewController.java
+++ src/com/projects/view/WorldViewController.java
@@ -1,6 +1,7 @@
 package com.projects.view;
 
 import com.projects.Main;
+import com.projects.helper.Constants;
 import com.projects.model.Sprite;
 import com.projects.model.Structure;
 import javafx.animation.AnimationTimer;
@@ -9,6 +10,7 @@ import javafx.fxml.FXML;
 import javafx.geometry.Rectangle2D;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.image.Image;
 import javafx.scene.input.MouseEvent;
 
 import java.util.List;
@@ -23,7 +25,9 @@ public class WorldViewController
 
     private GraphicsContext gc;
     private AnimationTimer animationTimer;
-    private final int selectionRange = 4;
+    private final int selectionRange = 16;
+    private Sprite selectionSprite = new Sprite(new Image(Constants.SELECTION_IMAGE), 0, 0);
+    private boolean selectionMade = false;
     private Main main;
 
     @FXML
@@ -38,7 +42,13 @@ public class WorldViewController
             @Override
             public void handle(long now)
             {
+                gc.clearRect(0, 0, worldCanvas.getWidth(), worldCanvas.getHeight());
                 List<Structure> worldStructures = main.getWorldStructureData();
+
+                if (selectionMade)
+                {
+                    gc.drawImage(selectionSprite.getImage(), selectionSprite.getXPosition(), selectionSprite.getYPosition());
+                }
 
                 for (Structure structure : worldStructures)
                 {
@@ -49,18 +59,22 @@ public class WorldViewController
         };
 
         animationTimer.start();
+        selectionMade = false;
     }
 
     @FXML
     private void handleMouseClick(MouseEvent mouseEvent)
     {
         List<Structure> worldStructures = main.getWorldStructureData();
-        Rectangle2D selectionRect = new Rectangle2D(mouseEvent.getX(), mouseEvent.getY(), mouseEvent.getX() + selectionRange, mouseEvent.getY() + selectionRange);
+        Rectangle2D selectionRect = new Rectangle2D(mouseEvent.getX() - selectionRange/2, mouseEvent.getY() - selectionRange/2, selectionRange, selectionRange);
 
         for (Structure structure : worldStructures)
         {
             if (structure.getSprite().intersects(selectionRect))
             {
+                selectionMade = true;
+                selectionSprite.setXPosition((structure.getSprite().getXPosition() + structure.getSprite().getImage().getWidth()/2) - selectionSprite.getImage().getWidth()/2);
+                selectionSprite.setYPosition((structure.getSprite().getYPosition() + structure.getSprite().getImage().getHeight()/2) - selectionSprite.getImage().getHeight()/2);
                 main.selectedStructureProperty().set(structure);
                 break;
             }
