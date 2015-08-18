14fca6b6d97d73d87899cd033876042a64afefc4
Dan Russell <danr94@gmail.com>
2015-08-18 14:16:01 -0700

    Removed images that are no longer used and added the new power plant images.

--- src/com/projects/helper/Constants.java
+++ src/com/projects/helper/Constants.java
@@ -11,8 +11,6 @@ public class Constants
     public static final String STRATEGIES_FILE_PATH = "./strategies/";
     public static final String TEMPLATE_FILE_PATH = "/data/SmartGridTemplate.xml";
     public static final String SELECTION_IMAGE = "/images/Selection.png";
-    public static final String HOUSE_IMAGE_PATH = "/images/House.png";
-    public static final String POWER_PLANT_IMAGE_PATH = "/images/PowerPlant.png";
     public static final String HOURS_AND_MINUTES_FORMAT = "HH:mm";
     public static final int IMAGE_SIZE = 32; // Width and height
     public static final long FIXED_SIMULATION_RATE_MILLISECONDS = 33; // 30 frames a second
